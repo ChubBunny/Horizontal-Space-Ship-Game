5e5c6da3af9a2e5057b5db1f264ed17a9269de73
ChubBunny <121910947+ChubBunny@users.noreply.github.com>
2023-01-10 16:57:46 -0800

    Update game design.py

--- game design.py
+++ game design.py
@@ -1,16 +1,22 @@
 import pygame
+from pygame.locals import(
+  K_UP, K_DOWN, K_RIGHT, K_LEFT, K_ESCAPE, KEYDOWN, QUIT
+)
 pygame.init()
-screen = pygame.display.set_mode([500,500])
-#A way of setting up boundaries for the game
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode([screen_width, screen_height])
 running = True
+surface = pygame.Surface((50, 50))
+surface.fill((255, 255, 255))
+rect = surface.get_rect()
+screen.blit(surface, (screen_width/2, screen_height/2))
+pygame.display.flip()
 while running:
-  #Because running is equal to true, andso we can stop the loop by setting the running variable to False
   for event in pygame.event.get():
-    #It will keep running as long as you do not tell it to stop
-    if event.type == pygame.QUIT: #QUIT = event
+    if event.type == KEYDOWN:
+      if event.type == K_ESCAPE:
+        running = False
+    elif event.type == QUIT:
       running = False
-  screen.fill((255, 255, 255))  
-  #Circle arguments: circle(location (screen), color (tuple) , x,y-location, radius)
-  pygame.draw.circle(screen, 'blue', (0,0), 20)
-  pygame.display.flip()
-pygame.quit() #quit() function
+class Player(pygame.sprite.Sprite):
